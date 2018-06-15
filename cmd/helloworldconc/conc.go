26d11e5e69f6e64f87d06b4ab63cf089db36ff0b
myway <myway99@sina.com>
2018-06-15 13:08:21 +0800

    new1

---
+++ cmd/helloworldconc/conc.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	ch := make(chan string)
+	for i := 0; i < 5000; i++ {
+		//go starts a goroutine
+		go printHelloWorld(i,ch)
+
+	}
+
+	for {
+		msg := <- ch
+		fmt.Println(msg)
+
+	}
+
+	//time.Sleep(time.Millisecond)
+}
+
+func printHelloWorld(i int, ch chan string) {
+	for {
+		ch <- fmt.Sprintf("Hello world from"+" goroutine %d !\n",i)
+	}
+
+
+}
---
+++ cmd/pipelinedemo/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"../../pipeline"
+	"os"
+	"bufio"
+)
+
+func main() {
+
+	const filename  = "small.in"
+	const n  = 64
+	
+	file,err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+
+	p := pipeline.RandomSource(n)
+
+	writer := bufio.NewWriter(file)
+	pipeline.WriterSink(writer,p)
+	writer.Flush()
+
+	file,err = os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	p = pipeline.ReaderSource(
+		bufio.NewReader(file),-1)
+	count := 0
+	for v := range p {
+		fmt.Println(v)
+		count ++
+		if count >= 100 {
+			break
+		}
+	}
+
+}
+
+func mergeDemo()  {
+
+	p := pipeline.Merge(
+		pipeline.InMemSort(pipeline.ArraySource(3, 2, 6, 7, 4,9,15)),
+		pipeline.InMemSort(pipeline.ArraySource(7, 4, 0, 3, 2,13,8,20,56)))
+	for v := range p {
+		fmt.Println(v)
+	}
+
+}
